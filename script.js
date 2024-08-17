b36c24bc085076fc7c27676a325888e0ea38ee11
Pintu Giri <mrpintugiri@gmail.com>
2024-08-17 20:18:58 +0530

    solution added

--- script.js
+++ script.js
@@ -8,20 +8,49 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+
+  arr.filter(person => person.profession === "developer")
+  .map(dev => console.log(dev.name));
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+
+  arr.forEach(person => {
+    if (person.profession === "developer") {
+      console.log(person.name);
+    }
+  });
+
 }
 
 function addData() {
   //Write your code here, just console.log
+
+  let newEmployee = { id: 4, name: "Pintu", age: "24", profession: "Developer 2" };
+  arr.push(newEmployee);
+  console.log(arr);
+
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+
+  arr = arr.filter(person => person.profession !== "admin");
+  console.log(arr);
+
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+
+  let newArray = [
+    { id: 5, name: "Nitin", age: "21", profession: "DBA" },
+    { id: 6, name: "Atul", age: "22", profession: "SDE1" },
+    { id: 7, name: "Akansha", age: "23", profession: "developer" }
+  ];
+
+  let concatenatedArray = arr.concat(newArray);
+  console.log(concatenatedArray);
+
 }
